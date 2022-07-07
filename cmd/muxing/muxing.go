f53f4113b22e80ef0b08d2305581fdab6414d987
Alexandr <alexandr.xomenko@gmail.com>
2022-07-07 19:54:55 +0300

    Add HandleFunc

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -21,6 +22,11 @@ main function reads host/port from env just for an example, flavor it following 
 func Start(host string, port int) {
 	router := mux.NewRouter()
 
+	router.HandleFunc("/name/{param}", NameHendler).Methods(http.MethodGet)
+	router.HandleFunc("/bad", BadHendler).Methods(http.MethodGet)
+	router.HandleFunc("/data", DataHendler).Methods(http.MethodPost)
+	router.HandleFunc("/headers", HeaderHendler).Methods(http.MethodPost)
+
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
 		log.Fatal(err)
@@ -35,4 +41,41 @@ func main() {
 		port = 8081
 	}
 	Start(host, port)
+}
+
+func NameHendler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	param := vars["param"]
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "Hello, %s", param)
+}
+
+func BadHendler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(500)
+}
+
+func DataHendler(w http.ResponseWriter, r *http.Request) {
+	param, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(500)
+	}
+	r.Body.Close()
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "I got message:\n %s", string(param))
+}
+
+func HeaderHendler(w http.ResponseWriter, r *http.Request) {
+	a := r.Header.Get("a")
+	b := r.Header.Get("b")
+	valA, err := strconv.Atoi(a)
+	if err != nil {
+		w.WriteHeader(500)
+	}
+	valB, err := strconv.Atoi(b)
+	if err != nil {
+		w.WriteHeader(500)
+	}
+	response := strconv.Itoa(valA + valB)
+	w.Header().Set("a+b", response)
+	w.WriteHeader(200)
 }
