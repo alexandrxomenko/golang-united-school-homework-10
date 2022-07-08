82a5dc50bd65b50a3786dbbda959f3a68491ff21
Alexandr <alexandr.xomenko@gmail.com>
2022-07-08 09:04:03 +0300

    Fix NameHandler

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -22,10 +22,10 @@ main function reads host/port from env just for an example, flavor it following 
 func Start(host string, port int) {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/name/{param}", NameHendler).Methods(http.MethodGet)
-	router.HandleFunc("/bad", BadHendler).Methods(http.MethodGet)
-	router.HandleFunc("/data", DataHendler).Methods(http.MethodPost)
-	router.HandleFunc("/headers", HeaderHendler).Methods(http.MethodPost)
+	router.HandleFunc("/name/{param}", NameHandler).Methods(http.MethodGet)
+	router.HandleFunc("/bad", BadHandler).Methods(http.MethodGet)
+	router.HandleFunc("/data", DataHandler).Methods(http.MethodPost)
+	router.HandleFunc("/headers", HeaderHandler).Methods(http.MethodPost)
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
@@ -43,28 +43,28 @@ func main() {
 	Start(host, port)
 }
 
-func NameHendler(w http.ResponseWriter, r *http.Request) {
+func NameHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	param := vars["param"]
 	w.WriteHeader(200)
-	fmt.Fprintf(w, "Hello, %s", param)
+	fmt.Fprintf(w, "Hello, %s!", param)
 }
 
-func BadHendler(w http.ResponseWriter, r *http.Request) {
+func BadHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(500)
 }
 
-func DataHendler(w http.ResponseWriter, r *http.Request) {
+func DataHandler(w http.ResponseWriter, r *http.Request) {
 	param, err := io.ReadAll(r.Body)
 	if err != nil {
 		w.WriteHeader(500)
 	}
 	r.Body.Close()
 	w.WriteHeader(200)
-	fmt.Fprintf(w, "I got message:\n %s", string(param))
+	fmt.Fprintf(w, "I got message:\n%s", string(param))
 }
 
-func HeaderHendler(w http.ResponseWriter, r *http.Request) {
+func HeaderHandler(w http.ResponseWriter, r *http.Request) {
 	a := r.Header.Get("a")
 	b := r.Header.Get("b")
 	valA, err := strconv.Atoi(a)
